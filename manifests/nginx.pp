3bd5cefa23b664b7ea0a23c64412d72504c13bd7
André de Jager <andrethehunter@gmail.com>
2015-07-17 10:38:25 +0100

    ran puppet-lint fixup, updated Vagrantfile

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,62 +1,62 @@
-/* nginx proxy pointing to tomcat on 8080 */
-class { "nginx": }
-nginx::resource::upstream { "railo":
-	ensure	=> present,
-	members	=> [
-		"localhost:8080",
-	],
+# nginx proxy pointing to tomcat on 8080
+class { 'nginx': }
+nginx::resource::upstream { 'railo':
+  ensure  => present,
+  members => [
+    'localhost:8080',
+  ],
 }
 
-/* nginx vhost using proxy + self signed ssl */
+# nginx vhost using proxy + self signed ssl
 nginx::resource::vhost {
-	"cp2.retailcloud.net":
-		ensure   => present,
-		proxy    => "http://railo",
-		ssl      => true,
-		ssl_cert => "/vagrant/conf/server.crt",
-		ssl_key  => "/vagrant/conf/server.key",
-		ssl_port => 443;
-	"monitor.local":
-		ensure   => present,
-		proxy    => "http://railo",
-		ssl      => true,
-		ssl_cert => "/vagrant/conf/server.crt",
-		ssl_key  => "/vagrant/conf/server.key",
-		ssl_port => 443;
+  'cp2.retailcloud.net':
+    ensure   => present,
+    proxy    => 'http://railo',
+    ssl      => true,
+    ssl_cert => '/vagrant/conf/server.crt',
+    ssl_key  => '/vagrant/conf/server.key',
+    ssl_port => 443;
+  'monitor.local':
+    ensure   => present,
+    proxy    => 'http://railo',
+    ssl      => true,
+    ssl_cert => '/vagrant/conf/server.crt',
+    ssl_key  => '/vagrant/conf/server.key',
+    ssl_port => 443;
 }
 
 nginx::resource::location {
-	'css/branding':
-		ensure   => present,
-		www_root => '/var/www/branding',
-		location => '/css/branding/',
-		vhost    => 'cp2.retailcloud.net',
-		ssl      => true;
-	'images/branding':
-		ensure   => present,
-		www_root => '/var/www/branding',
-		location => '/images/branding/',
-		vhost    => 'cp2.retailcloud.net',
-		ssl      => true;
-	'favicon.ico':
-		ensure   => present,
-		www_root => '/var/www/branding',
-		location => '/favicon.ico',
-		vhost    => 'cp2.retailcloud.net',
-		ssl      => true;
+  'css/branding':
+    ensure   => present,
+    www_root => '/var/www/branding',
+    location => '/css/branding/',
+    vhost    => 'cp2.retailcloud.net',
+    ssl      => true;
+  'images/branding':
+    ensure   => present,
+    www_root => '/var/www/branding',
+    location => '/images/branding/',
+    vhost    => 'cp2.retailcloud.net',
+    ssl      => true;
+  'favicon.ico':
+    ensure   => present,
+    www_root => '/var/www/branding',
+    location => '/favicon.ico',
+    vhost    => 'cp2.retailcloud.net',
+    ssl      => true;
 }
 
 nginx::resource::location {
-	'jpg_rgb_150px':
-		ensure   => present,
-		www_root => '/var/client-filestore/CPM_ConcretePlatform',
-		location => '/jpg_rgb_150px',
-		vhost    => 'cp2.retailcloud.net',
-		ssl      => true;
-	'jpg_rgb_500px':
-		ensure   => present,
-		www_root => '/var/client-filestore/CPM_ConcretePlatform',
-		location => '/jpg_rgb_500px',
-		vhost    => 'cp2.retailcloud.net',
-		ssl      => true;
+  'jpg_rgb_150px':
+    ensure   => present,
+    www_root => '/var/client-filestore/CPM_ConcretePlatform',
+    location => '/jpg_rgb_150px',
+    vhost    => 'cp2.retailcloud.net',
+    ssl      => true;
+  'jpg_rgb_500px':
+    ensure   => present,
+    www_root => '/var/client-filestore/CPM_ConcretePlatform',
+    location => '/jpg_rgb_500px',
+    vhost    => 'cp2.retailcloud.net',
+    ssl      => true;
 }
--- manifests/railo.pp
+++ manifests/railo.pp
@@ -1,64 +1,64 @@
 # railo.pp
 
-/* define the railo version you want here: */
-$railoVersion = "4.1.2.005"
+# define the railo version you want here:
+$railoVersion = '4.1.2.005'
 $warFileName = "railo-${railoVersion}.war"
 
-/* update all packages */
-exec { "apt-get":
-	command	=> "/usr/bin/apt-get update",
+# update all packages
+exec { 'apt-get':
+  command  => '/usr/bin/apt-get update',
 }
 
-/* get htop */
-package { "htop":
-	ensure	=> present,
-	require	=> Exec["apt-get"],
+# get htop
+package { 'htop':
+  ensure  => present,
+  require => Exec['apt-get'],
 }
 
-package { "curl":
-	ensure	=> present,
-	require	=> Exec["apt-get"],
+package { 'curl':
+  ensure  => present,
+  require => Exec['apt-get'],
 }
 
-/* add cp2 to to the hosts file */
-host { "cp2.retailcloud.net":
-	ip	=> "127.0.0.1",
+# add cp2 to to the hosts file
+host { 'cp2.retailcloud.net':
+  ip  => '127.0.0.1',
 }
 
 
-/* make sure tomcat is installed */
-package {"tomcat7":
-	ensure	=> installed,
-	require	=> Package["curl"],
+# make sure tomcat is installed
+package {'tomcat7':
+  ensure  => installed,
+  require => Package['curl'],
 }
 
-/* once tomcat is installed */
-service { "tomcat7":
-	ensure  => "running",
-	enable  => "true",
-	require => Package["tomcat7"],
+# once tomcat is installed
+service { 'tomcat7':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['tomcat7'],
 }
 
-/* download the railo war */
-exec { "railo-download": 
+# download the railo war
+exec { 'railo-download':
   command => "/usr/bin/wget --quiet http://www.getrailo.org/railo/remote/download/${railoVersion}/custom/all/${warFileName}",
-  cwd => "/home/vagrant/",
+  cwd     => '/home/vagrant/',
   creates => "/home/vagrant/${warFileName}",
   require => Package['tomcat7'],
 }
 
-/* stick the railo war file into tomcat */
-file { "railo.war":
-	name	=> "/var/lib/tomcat7/webapps/railo.war",
-	owner	=> "tomcat7",
-	group	=> "tomcat7",
-	source	=> "/home/vagrant/${warFileName}",
-	require	=> Exec["railo-download"],
+# stick the railo war file into tomcat
+file { 'railo.war':
+  name    => '/var/lib/tomcat7/webapps/railo.war',
+  owner   => 'tomcat7',
+  group   => 'tomcat7',
+  source  => "/home/vagrant/${warFileName}",
+  require => Exec['railo-download'],
 }
 
-/* prod tomcat */
-exec { "curl":
-	name		=> "/usr/bin/curl localhost:8080",
-	require		=> File["railo.war"],
-	notify		=> Service["tomcat7"],
+# prod tomcat
+exec { 'curl':
+  name    => '/usr/bin/curl localhost:8080',
+  require => File['railo.war'],
+  notify  => Service['tomcat7'],
 }
